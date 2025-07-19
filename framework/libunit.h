2d13e37a2d573e7cc1e063605e7142a44604f1ae
RamaSham <rsham@rama.localdomain>
2025-07-19 10:18:14 +0300

    added libft to tests

--- framework/libunit.h
+++ framework/libunit.h
@@ -21,7 +21,7 @@ typedef struct s_unit_test
 
 
 void	load_test(t_unit_test **list, char *name, int (*func)(void));
-int	launch_tests(t_unit_test **list, char *test_function);
+int		launch_tests(t_unit_test **list, char *test_function);
 void 	free_tests(t_unit_test *list);
 void	print_signal_result(int status);
 size_t	ft_strlen(char const *s);
